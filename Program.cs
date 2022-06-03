dce634a3942c5aca34e3cfb316e5b2f705f45d0c
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-06-03 09:02:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = height - i - 1; k >= 0; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int s = 2 * i + 1; s > 0; s--)
+                {
+                    Console.Write("*");
+                }
+
+                
+
+                Console.WriteLine();
+                
+            }
+            
+
+        }
+
+        
+        
+        
+       
+    }
+}
